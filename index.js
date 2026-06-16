f2cb94b5a71a988cf8c7cebbf3d6edffd074eccb
Rajat Sharma <rajatsharmaw90gmail.com>
2026-06-16 06:45:54 -0700

    Initial website deployment

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.post("/api/contact", async (req, res) => {
+  const { name, email, service, message } = req.body;
+
+  try {
+    await pool.query(
+      `
+      INSERT INTO contacts
+      (name, email, message)
+      VALUES ($1,$2,$3)
+      `,
+      [name, email, message]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Contact saved",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
